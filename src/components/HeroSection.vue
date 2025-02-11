42d93b9c513768554a680ae486e3deaf5e595da9
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2025-02-11 15:02:10 +0100

    feat: update hero section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -8,12 +8,14 @@
       </h1>
       <p class="main-desc">GET TO KNOW THE FUTURE PIRATES KING</p>
     </div>
+    <img class="gear-5" src="/gear_5_luffy.png" alt="gear 5 luffy one piece" />
   </section>
 </template>
 
 <style scope>
 .wrapper {
   position: relative;
+  overflow: hidden;
 }
 
 .main-bg {
@@ -22,11 +24,9 @@
 
 .main-sect {
   position: absolute;
-  top: 0px;
-  left: 0px;
+  top: 120px;
+  right: 70px;
   display: flex;
-  width: 100%;
-  height: 100%;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -35,9 +35,9 @@
 
 .main-head {
   position: relative;
-  font-size: 128px;
+  font-size: 8rem;
   font-weight: 700;
-  line-height: 128px;
+  line-height: 8rem;
   color: white;
   font-family: 'Poppins';
 }
@@ -49,9 +49,55 @@
 }
 
 .main-desc {
-  font-size: 31px;
+  font-size: 2rem;
   font-weight: 600;
   color: white;
   font-family: 'Poppins';
+}
+
+.gear-5 {
+  position: absolute;
+  bottom: -100px;
+  right: 0;
+  animation-name: Bounce;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+
+@keyframes Bounce {
+  0% {
+    transform: translateY(0) rotate(5deg);
+  }
+
+  100% {
+    transform: translateY(-50px) rotate(0deg);
+  }
+}
+
+@media (max-width: 1366px) {
+  .gear-5 {
+    width: 600px;
+  }
+  .main-head {
+    font-size: 7rem;
+    line-height: 7rem;
+  }
+  .main-desc {
+    font-size: 1.5rem;
+  }
+}
+
+@media (min-width: 1920px) {
+  .gear-5 {
+    width: 850px;
+  }
+  .main-head {
+    font-size: 11rem;
+    line-height: 11rem;
+  }
+  .main-desc {
+    font-size: 2.6rem;
+  }
 }
 </style>
