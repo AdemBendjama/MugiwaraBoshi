19ae87f8751daf71fc33998952fa2c32bc63cd93
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2025-02-12 16:25:58 +0100

    build: install and setup tailwind v4, install shadcn-vue, migrate from tailwind v3 to v4

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,45 +1,39 @@
 <script setup lang="ts">
 import HeroHeading from './HeroHeading.vue'
 import PlayVideo from './PlayVideo.vue'
+// import Button from './ui/button/Button.vue'
 </script>
 
 <template>
   <section class="wrapper">
     <img class="bg" src="/main_bg.png" alt="straw hat luffy" />
     <HeroHeading />
-    <img class="gear-5" src="/gear_5_luffy.png" alt="gear 5 luffy one piece" />
+    <img class="gear-5 animate-gear-5" src="/gear_5_luffy.png" alt="gear 5 luffy one piece" />
     <PlayVideo />
   </section>
+  <!-- <div class="h-96">
+    <h1>Hello world</h1>
+    <Button>Click me Please</Button>
+  </div> -->
 </template>
 
 <style scope>
 .wrapper {
   position: relative;
   overflow: hidden;
+  height: 100vh;
 }
 
 .bg {
   width: 100%;
+  height: 100vh;
+  object-fit: cover;
 }
 
 .gear-5 {
   position: absolute;
-  bottom: -100px;
-  right: 0;
-  animation-name: Bounce;
-  animation-duration: 2s;
-  animation-iteration-count: infinite;
-  animation-direction: alternate;
-}
-
-@keyframes Bounce {
-  0% {
-    transform: translateY(0) rotate(5deg);
-  }
-
-  100% {
-    transform: translateY(-50px) rotate(0deg);
-  }
+  bottom: 0;
+  right: 30px;
 }
 
 @media (max-width: 1366px) {
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+import animate from "tailwindcss-animate"
+
+module.exports = {
+  darkMode: ["class"],
+  safelist: ["dark"],
+  prefix: "tw",
+
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+  ],
+
+  theme: {
+    extend: {
+
+      borderRadius: {
+        xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+        "collapsible-down": {
+          from: { height: 0 },
+          to: { height: 'var(--radix-collapsible-content-height)' },
+        },
+        "collapsible-up": {
+          from: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-down": "collapsible-down 0.2s ease-in-out",
+        "collapsible-up": "collapsible-up 0.2s ease-in-out",
+      },
+    },
+  },
+  plugins: [animate],
+}
