4442775bd15dc4e36bef5bc49412548d29a0719e
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2025-02-12 17:24:16 +0100

    feat: add image

--- src/components/PlayVideo.vue
+++ src/components/PlayVideo.vue
@@ -1,61 +1,9 @@
-<script setup lang="ts">
-import PlayButton from './svgs/PlayButton.vue'
-</script>
-
 <template>
-  <div class="video">
-    <div class="video-wrapper">
-      <img class="video-img" src="/video_bg.png" alt="background asset" />
-      <div class="video-content">
-        <div class="content-container">
-          <img src="/one_piece_logo.png" alt="One Piece logo" />
-          <p class="content-desc">FAN WEBSITE</p>
-        </div>
-        <PlayButton />
-      </div>
-    </div>
+  <div class="absolute bottom-14 left-11">
+    <img
+      class="3xl:w-[525px] 3xl:h-[200px]"
+      src="/play_video.png"
+      alt="onepiece logo next to a play video button"
+    />
   </div>
 </template>
-
-<style scoped>
-.video {
-  position: absolute;
-  bottom: 56px;
-  left: 46px;
-}
-
-.video-wrapper {
-  position: relative;
-  height: 148px;
-  width: 389px;
-}
-
-.video-content {
-  height: 100%;
-  width: 100%;
-  padding: 16px 24px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.video-content .content-container {
-  display: flex;
-  flex-direction: column;
-  gap: 6px;
-  justify-content: center;
-  align-items: center;
-}
-
-.video-content .content-desc {
-  font-size: 14px;
-  color: #d92332;
-  font-family: 'Poppins';
-  font-weight: 700;
-}
-
-.video-img {
-  position: absolute;
-  object-fit: cover;
-}
-</style>
