c9d05793fd4789e88a08d2eb1740761f6bd0a9ee
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2025-02-16 09:51:59 +0100

    style: add responsive

--- src/components/sections/StorySection.vue
+++ src/components/sections/StorySection.vue
@@ -6,7 +6,7 @@ import CarouselItem from '../ui/carousel/CarouselItem.vue'
 </script>
 
 <template>
-  <div class="h-screen flex flex-col items-center justify-between">
+  <div class="h-screen flex flex-col gap-4 items-center justify-between">
     <Carousel class="px-7">
       <CarouselContent class="-ml-16">
         <CarouselItem
@@ -14,22 +14,30 @@ import CarouselItem from '../ui/carousel/CarouselItem.vue'
           :key="index"
           class="basis-1/5 py-9 pl-16 flex justify-center"
         >
-          <img :src="`/crew/avatars/crew_${index + 1}.jpg`" alt="crew member of straw pirates" />
+          <img
+            class="3xl:w-[300px] 3xl:h-[300px] 2xl:w-[230px] 2xl:h-[230px]"
+            :src="`/crew/avatars/crew_${index + 1}.jpg`"
+            alt="crew member of straw pirates"
+          />
         </CarouselItem>
       </CarouselContent>
     </Carousel>
-    <img src="/straw_hats.png" alt="straw hats logo skulls" />
+    <img
+      class="3xl:w-96 3xl:h-32 2xl:w-72 2xl:h-24 w-64 h-20"
+      src="/straw_hats.png"
+      alt="straw hats logo skulls"
+    />
     <div class="relative w-full h-[350px] flex items-center justify-center px-80">
       <CustomButton class="absolute -top-12 left-36 -rotate-1">The Story</CustomButton>
       <p
-        class="uppercase text-[22px] leading-8 tracking-widest font-bold text-primary-foreground text-center text-balance"
+        class="uppercase 3xl:text-3xl 2xl:text-[22px] text-lg leading-relaxed tracking-widest font-bold text-primary-foreground text-center text-balance"
       >
         the captain Straw-hat Monkey.D.Luffy and his crew members sail out to the sea fighting the
         world.GOV and formidable pirate Enemies To find the former pirate king’s tressure The one
         piece
       </p>
       <img
-        class="absolute -z-10 bottom-0 left-0 w-full object-cover"
+        class="absolute -z-10 bottom-0 left-0 object-cover w-full h-full"
         src="/red_bg.png"
         alt="red background"
       />
